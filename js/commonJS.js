f1717db5d06115ec575f79fd595c79e4d6a718c8
mdwork <mazdze@gmail.com>
2015-03-17 00:20:39 +0200

    mib

--- js/commonJS.js
+++ js/commonJS.js
@@ -17,4 +17,40 @@ $(document).ready(function(){
         before: function(){},   // Function: Before callback
         after: function(){}     // Function: After callback
     });
+
+    $('.slides-js').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
